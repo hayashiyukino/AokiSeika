795c6a15df49c042219bc6d17bea9c15ff75a789
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-12-19 06:17:31 +0000

    [add]adominジャンルコントロール作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admins = [
+   {email: 'aoki@seika', password: '789789'},
+]
+
+admins.each do |admin|
+   admin_data = Admin.find_by(email: admin[:email])
+   if admin_data.nil?
+      Admin.create(email: admin[:email], password: admin[:password])
+   end
+end
