8c56b1b72aa510074ecb636f8e083cc4209b540b
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-12-20 17:29:28 +0000

    [add]adminジャンル一覧ページ作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,42 +1,45 @@
 class Admin::GenresController < ApplicationController
   # before_action :autheticate_admin!
+  before_action :ensure_genre, only: [:show, :edit, :update]
+
 
   def index
-    @genres = Genre.all
     @genre = Genre.new
+    @genres = Genre.all
   end
 
-  def edit
-    @genre = Genre.find(params[:id])
-  end
-
-  def update
-    @genre = Genre.find(params[:id])
-    if @genre.update(genre_params)
-      flash[:notice] = "ジャンルの更新に成功しました"
-      redirect_to admin_genres_path
-    else
-      flash[:notice] = "ジャンルの更新に失敗しました"
-      render :edit
-    end
-  end
-
-  def creat
+  def create
     @genre = Genre.new(genre_params)
     if @genre.save
-      flash[:notice] = "ジャンルの登録に成功しました"
       redirect_to admin_genres_path
     else
       @genres = Genre.all
-      flash[:notice] = "ジャンルの登録に失敗しました"
       render :index
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @genre.update(genre_params)
+      unless @genre.is_active
+        @genre.items.update_all(is_active: false)
+      end
+      redirect_to admin_genres_path
+    else
+      render :edit
     end
   end
 
   private
 
   def genre_params
-    params.require(:genre).permit(:name)
+    params.require(:genre).permit(:name, :is_active)
+  end
+
+  def ensure_genre
+    @genre = Genre.find(params[:id])
   end
 
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,7 @@
 class Genre < ApplicationRecord
   has_many :items, dependent: :destroy
+  
+  scope :only_active, -> { where(is_active: true) }
 
-  # validates :name, presemce: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
