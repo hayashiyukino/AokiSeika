96f2b823516356e6d5a320f6a6daa0391fbdc8d2
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-12-22 14:26:57 +0000

    model作成

--- db/migrate/20221221143402_create_addresses.rb
+++ db/migrate/20221221143402_create_addresses.rb
@@ -4,9 +4,7 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
       t.integer :customer_id, null: false
     	t.string :name, null: false
       t.string :postal_code, null: false
-      t.string :address, null: fa
-    	t.string :destination, null: false
-      t.timestamps
+      t.string :address, null: false
       t.timestamps
     end
   end
