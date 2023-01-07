f54a3626c34a315f8e9e617814753344759bc8a4
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2023-01-07 14:59:18 +0000

    [add]items_controller追記

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,15 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
-
   def new
+    @item = Item.new
   end
 
-  def show
+  def create
+    @item = Item.new(params)
+    @item.save
+    redirect_to admin_items_path
   end
 
-  def edit
+  def params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
